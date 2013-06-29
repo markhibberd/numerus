f8e577e7da0b5c29f8ed21b7275ecd62edac1e7a
markhibberd <mark@hibberd.id.au>
2013-06-29 20:00:18 +1000

    Heroku port.

--- src/Numerus/Core.hs
+++ src/Numerus/Core.hs
@@ -5,8 +5,8 @@ import Web.Scotty
 
 import Data.Monoid (mconcat)
 
-defaultMain :: IO ()
-defaultMain = scotty 3000 $ do
+defaultMain :: Int -> IO ()
+defaultMain port = scotty port $ do
   get "/:word" $ do
     beam <- param "word"
     html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
--- src/main.hs
+++ src/main.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 import Numerus
+import System.Environment
+import Data.List
+import Data.Maybe
+import Control.Applicative
 
 main :: IO ()
-main = defaultMain
+main =
+  do env <- getEnvironment
+     let port = fromIntegral . (read :: String -> Int) . snd <$> (find (((==) "PORT") . fst) env)
+     defaultMain $ fromMaybe 3000 port
